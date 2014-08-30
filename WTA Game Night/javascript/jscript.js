c3f3cdb5bfb30d106aa91c5dabe838082a33fa2e
roney17 <hunteroney@gmail.com>
2014-08-29 19:04:17 -0600

    Initial Commit
    First One Yo

---
+++ WTA Game Night/javascript/jscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $("#whenAndWhere").mouseenter(function(){
+    $(".Here").slideDown(1500);
+
+  });
+  	   $("#whenAndWhere").mouseleave(function(){
+     $(".Here").slideUp(1500);
+ });
+
+
+});
+
+
